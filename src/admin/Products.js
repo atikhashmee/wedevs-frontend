f8ccdddf7df4275f544659e8d2fb59be5f96c00e
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-04-26 17:36:58 +0600

    Update Products.js

--- src/admin/Products.js
+++ src/admin/Products.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const Products = () => {
     return ( <>
+
+
         <h1>Product page</h1>
+        <ProductLists />
     </> );
+}
+
+const ProductLists = () =>{
+        return (
+            <table className="table table-bordered border-primary">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                 <tbody>
+                     {Array(5).fill(0).map((it,idd)=>(
+                         <tr key={idd}>
+                             <td>{idd}</td>
+                             <td>{"name "+idd}</td>
+                         </tr>
+                     ))}
+                 </tbody>
+          </table>
+        );
 }
  
 export default Products;
